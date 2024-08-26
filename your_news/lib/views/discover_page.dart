219b626084892f62750370d844c011b964432107
sammy-sasa <151463535+sammysamuti@users.noreply.github.com>
2024-08-26 18:09:12 +0300

    Added: dicover page done

--- your_news/lib/views/discover_page.dart
+++ your_news/lib/views/discover_page.dart
@@ -1,63 +1,404 @@
 import 'package:flutter/material.dart';
-import 'package:get_storage/get_storage.dart';
-import 'package:your_news/core/widgets/bottom_navigation_bar.dart';
-import 'package:your_news/core/widgets/drawer.dart';
-import 'package:your_news/core/widgets/custom_app_bar.dart';
 import 'package:your_news/models/news_model.dart';
+import 'package:your_news/views/recommendation_detail.dart';
 
 class DiscoverPage extends StatefulWidget {
   @override
-  _DiscoverPage createState() => _DiscoverPage();
+  _DiscoverPageState createState() => _DiscoverPageState();
 }
 
-class _DiscoverPage extends State<DiscoverPage> {
-  int _currentIndex = 0;
-  String username = '';
+class _DiscoverPageState extends State<DiscoverPage> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  String _selectedCategory = 'All';
 
-  @override
-  void initState() {
-    super.initState();
-    _loadUsername();
-  }
-
-  void _loadUsername() {
-    final GetStorage storage = GetStorage();
-    final email = storage.read('email') ?? '';
-    final parts = email.split('@');
-    if (parts.isNotEmpty) {
-      setState(() {
-        username = parts[0];
-      });
-    }
-  }
+  final List<String> _categories = [
+    'All',
+    'Recommended',
+    'Popular',
+    'Most Visited',
+    'Trending',
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: _scaffoldKey,
-      appBar: CustomAppBar(
-        showMenu: true,
-        showNotification: true,
-        showSettings: false,
-        showVerts: false,
-        search: false,
-        showBackButton: true,
-        title: '',
-        onMenuPressed: () {
-          _scaffoldKey.currentState?.openDrawer();
-        },
-        onNotificationPressed: () {
-          print('Notification pressed');
-        },
-        onSettingsPressed: () {},
-        onVertsPressed: () {},
-        onBackPressed: () {
-          Navigator.of(context).pop(); 
-        },
+      appBar: AppBar(
+        title: Text(
+          'Discover',
+          style: TextStyle(
+            fontSize: 24.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
       ),
-      drawer: DrawerWidget(context: context),
-      body: SingleChildScrollView(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(16.0),
+              child: Text(
+                'Uncover a world of captivating stories and stay informed',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black,
+                  height: 1.5,
+                ),
+                textAlign: TextAlign.left,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                color: Color(0xFFF9FCFE),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'Search "News"',
+                  prefixIcon: Icon(Icons.search, color: Colors.grey),
+                ),
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Publishers',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  SizedBox(
+                    height: 160,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 2,
+                      itemBuilder: (context, index) {
+                        final imagePaths = [
+                          'assets/images/bbc.jpeg',
+                          'assets/images/fox_news.jpeg',
+                        ];
+                        return Container(
+                          width: 150,
+                          margin: EdgeInsets.symmetric(horizontal: 8),
+                          decoration: BoxDecoration(
+                            color: Color(0xFFF9FCFE),
+                            borderRadius: BorderRadius.circular(12.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.3),
+                                spreadRadius: 2,
+                                blurRadius: 5,
+                                offset: Offset(0, 3),
+                              ),
+                            ],
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(top: 16.0),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(8.0),
+                                  child: Image.asset(
+                                    imagePaths[index % imagePaths.length],
+                                    fit: BoxFit.cover,
+                                    height: 60,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(height: 8),
+                              Text(
+                                'BBC News', 
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.symmetric(
+                                    vertical: 12.0, horizontal: 16.0),
+                                decoration: BoxDecoration(
+                                  color: Colors.grey[200],
+                                  borderRadius: BorderRadius.circular(8.0),
+                                ),
+                                child: Text(
+                                  'Follow',
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'News',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  SizedBox(
+                    height: 50,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: _categories.length,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                          child: _categoryButton(_categories[index]),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+              child: Column(
+                children: [
+                  _buildRecommendation(
+                    NewsModel(
+                      category: 'Business',
+                      title:
+                          'Tech Startup Secures \$50 Million Funding for Expansion',
+                      news: 'Forbes',
+                      date: 'Jun 11, 2023',
+                      imagePath: 'assets/images/nature2.jpeg',
+                      icon: 'assets/images/forbeds.jpeg',
+                      icon2: 'assets/images/verified.png',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _categoryButton(String category) {
+    bool isSelected = _selectedCategory == category;
+
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _selectedCategory = category;
+        });
+      },
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            child: Text(
+              category,
+              style: TextStyle(
+                color: isSelected ? Colors.blue : Colors.grey,
+                fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+              ),
+            ),
+          ),
+          if (isSelected) SizedBox(height: 4.0),
+          if (isSelected)
+            Container(
+              width: 8.0,
+              height: 8.0,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                shape: BoxShape.circle,
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildRecommendation(NewsModel recommendation) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => RecommendationDetail(
+              recommendation: recommendation,
+            ),
+          ),
+        );
+      },
+      child: Card(
+        color: Color(0xFFF9FCFE),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(4),
+                          child: Image.asset(
+                            recommendation.icon!,
+                            width: double.infinity,
+                            height: 48,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  Expanded(
+                    flex: 4,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              '${recommendation.news}',
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.grey[600],
+                              ),
+                            ),
+                            SizedBox(width: 3),
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(4),
+                              child: Image.asset(
+                                recommendation.icon2!,
+                                width: 24,
+                                height: 24,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 4),
+                        Text(
+                          recommendation.date!,
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey[400],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Row(
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.grey[300],
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              minimumSize: Size(100, 36),
+                            ),
+                            child: Text(
+                              'Follow',
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          ),
+                          SizedBox(width: 8),
+                          Icon(Icons.more_vert,
+                              size: 24, color: Colors.grey[600]),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 8),
+            Text(
+              recommendation.title!,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+            SizedBox(height: 8),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => RecommendationDetail(
+                      recommendation: recommendation,
+                    ),
+                  ),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  side: BorderSide(
+                    color: Colors.blue,
+                    width: 1,
+                  ),
+                ),
+                minimumSize: Size(100, 36),
+              ),
+              child: Text(
+                'Business',
+                style: TextStyle(color: Colors.blue),
+              ),
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: Image.asset(
+                recommendation.imagePath!,
+                width: double.infinity,
+                height: 150,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
